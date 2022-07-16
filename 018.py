f104c127bec5cc315a708b1e1769dc025329bbe3
Kisho LinuxMac <kisokumar886@gmail.com>
2022-07-16 11:37:03 +0100

    solving 18.py

---
+++ 018.py
@@ -0,0 +1,12 @@
+# Problem 18 - Maximum path sum I
+# Find the maximum total path from top to bottom of the triangle below
+
+def sol():
+    ans = 0
+
+    return ans
+
+if __name__ == '__main__':
+    print(sol())
+
+# ans = 0
