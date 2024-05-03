7b34d673cc6b3c4a1eb1f985c31b42d46d4be0a7
k16n <k16n@mbp.local>
2024-05-04 00:41:38 +0100

    add: cpp template

---
+++ template.cpp
@@ -0,0 +1,17 @@
+// Problem x - Title
+// Desc
+
+#include <iostream>
+
+int sol(int num) {
+    return num;
+}
+
+int main() {
+    int outputNumber = sol(5);
+    std::cout << "Solution: " << outputNumber << std::endl;
+
+    return 0;
+}
+
+// ans = 0
